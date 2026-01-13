5fd081192362374028c8a31e0bae8b93212c1f7e
Vinicios Barboza <vinicios12345barboza@gmail.com>
2026-01-12 21:18:16 -0300

    atualizei

--- exercicios/estudo/desafio05/Guts_files/polyfills-c311ed1218cc599b.mjs
+++
@@ -1,2 +0,0 @@
-(self.modernJsonp=self.modernJsonp||[]).push([["85190"],{842718(){}},function(n){n(n.s=842718)}]);
-//# sourceMappingURL=https://sm.pinimg.com/webapp/polyfills-c311ed1218cc599b.mjs.map
